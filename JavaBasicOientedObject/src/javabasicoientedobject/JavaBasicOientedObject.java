b29bafaf320fda63d77ea3ff97a2b4e300979523
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2023-08-08 18:58:19 -0300

    add-01: first commit of repository.

---
+++ JavaBasicOientedObject/src/javabasicoientedobject/JavaBasicOientedObject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabasicoientedobject;
+
+/**
+ *
+ * @author maumneto
+ */
+public class JavaBasicOientedObject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World from Oriented Object!");
+    }
+    
+}
